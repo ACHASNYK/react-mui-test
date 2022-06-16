25c4be00e63282952361494349d2f342065a3be7
Achasnyk <a.chasnyk@gmail.com>
2022-06-16 12:30:10 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import { Data } from './data';
+// import './App.css';
 
 function App() {
+  const headerRow1 = Object.keys(Data.Kyivska.G);
+    console.log(headerRow1)// Do whatever you like with key and value
+  // const headerRow2 = Object.keys(Data.Kyivska.G.)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Paper sx={{ width: '100%' }}>
+      <TableContainer sx={{ maxHeight: 500, maxWidth: 1500 }}>
+        <Table stickyHeader aria-label='sticky table'>
+          <TableHead>
+            <TableRow>
+              <TableCell align='center' colSpan={1}></TableCell>
+              {headerRow1.map((e) => (
+              
+              
+              <TableCell align='center' colSpan={3}>{e}</TableCell>
+              
+              )
+            )  
+            }
+            </TableRow>
+            <TableRow>
+              <TableCell align='center' colSpan={1}>
+                    "Regions"
+                  </TableCell>
+              {Object.keys(Data.Kyivska.G[2017]).map((e) =>(
+                            
+                  <TableCell align='center'>{e}</TableCell> 
+              
+              )
+              )}
+            </TableRow>
+          </TableHead>
+        </Table>
+      </TableContainer>
+      
+    </Paper>
   );
 }
 
